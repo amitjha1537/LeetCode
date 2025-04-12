9e1389b5d8dd9e75adf7868eca71f23d6df6cb69
amitjha1537 <166430182+amitjha1537@users.noreply.github.com>
2025-04-12 18:12:24 +0530

    Time: 1 ms (99.9%), Space: 43.5 MB (78.22%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    private void back(int ind, int[] arr,List<List<Integer>> ans, List<Integer>ds){
+            ans.add(new ArrayList<>(ds) );
+           
+        
+        for(int i=ind;i<arr.length;i++){
+            if(i>ind&&arr[i]==arr[i-1]) continue;
+            ds.add(arr[i]);
+            back(i+1,arr,ans,ds);
+            ds.remove(ds.size()-1);
+           
+        }
+        
+        
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+         List<List<Integer>> ans=new ArrayList<>();
+        back(0,nums,ans,new ArrayList<>());
+        return ans;
+        
+    }
+
+
+    
+    
+
+        
+    
+}

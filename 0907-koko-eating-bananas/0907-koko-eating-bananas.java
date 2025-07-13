41682e91d44325caa85e1e945a566ef1d86f9deb
amitjha1537 <166430182+amitjha1537@users.noreply.github.com>
2025-07-13 14:19:08 +0530

    Time: 21 ms (47.49%), Space: 45.4 MB (24.23%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,29 +1,32 @@
 class Solution {
-    private boolean perh(int[] piles,int h, int k){
-        int hours=0;
-        for(int i=0;i<piles.length;i++){
-            hours+=(int)Math.ceil((double)piles[i]/k);
-        }
-        return hours<=h;
+    private boolean check(int []arr, int mid,int h){
+        int hrs=0;
+        for(int i=0;i<arr.length;i++){
+            hrs+=(int) Math.ceil((double)arr[i]/mid);
 
+        }
+        return hrs<=h;
     }
     public int minEatingSpeed(int[] piles, int h) {
+       
         int left=1;
         int right=Integer.MAX_VALUE;
-        int result=right;
         
+        
+        int ans=0;
         while(left<=right){
+            
             int mid=left+(right-left)/2;
-           if(perh(piles,h,mid)){
 
-           result=mid;
-           
-            right=mid-1;
-           }            
-           else{
-            left=mid+1;
-           }
+            if(check(piles,mid,h)==true){
+                ans=mid;
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
         }
-        return result;
+        return ans;
+        
     }
 }

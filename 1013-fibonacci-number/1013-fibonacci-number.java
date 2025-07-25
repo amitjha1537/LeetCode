fe351421c2c2b15d7e0301a8ab9bfe381cc9ecda
amitjha1537 <166430182+amitjha1537@users.noreply.github.com>
2025-07-25 18:23:04 +0530

    Time: 0 ms (100%), Space: 40.6 MB (37.43%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,13 +1,17 @@
 class Solution {
-    private int f(int n){
+    private int f(int n,Map<Integer,Integer> memo){
         if(n<=1) return n;
-      int   last=f(n-1);
-        int slast=f(n-2);
-        return last+slast;
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+      int result= f(n-1,memo)+f(n-2,memo);
+    memo.put(n,result);
+        return result;
 
     }
     public int fib(int n) {
-        return f(n);
+        HashMap<Integer,Integer> memo=new HashMap<>();
+        return f(n,memo);
         
     }
 }

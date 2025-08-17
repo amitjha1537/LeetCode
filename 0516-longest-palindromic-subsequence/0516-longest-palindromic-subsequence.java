e25ab68039366478b7ac45e392bec9a1f04e1538
amitjha1537 <166430182+amitjha1537@users.noreply.github.com>
2025-08-17 12:52:49 +0530

    Time: 57 ms (38.32%), Space: 55.1 MB (72.52%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,30 +1,22 @@
 class Solution {
-    private int[][] dp;
-    private int check(String s, int i, int j){
-        if(i>j){
-            return 0;
-        }
-        if(i==j) return 1;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s.charAt(i)==s.charAt(j)){
-            dp[i][j]=2+check(s,i+1,j-1);
-        }
-        else{
-            dp[i][j]= Math.max(check(s,i+1,j),check(s,i,j-1));
-        }
-        return dp[i][j];
-    }
     public int longestPalindromeSubseq(String s) {
-        int n=s.length();
-         dp=new int[n][n];
-         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
+        // in java if you to initialize the row 1 and column 1 by 0 
+      //  there is no need as in java by default in places are occupied by 0
+      // but in good practice you should do, so in case u have do it otherwise so you should be prepared
+      String rev=new StringBuilder(s).reverse().toString();
+      int n=s.length();
+      int[][] dp=new int[n+1][n+1];
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(s.charAt(i-1)==rev.charAt(j-1)){
+                dp[i][j]=1+dp[i-1][j-1];
             }
-         }
-         return check(s,0,n-1);
+            else{
+                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+      } 
+      return dp[n][n];
         
     }
 }

ae8639098ea49865f1a7f8c0c02e67c229ae1fd3
amitjha1537 <166430182+amitjha1537@users.noreply.github.com>
2025-08-25 09:59:29 +0530

    Time: 2 ms (66.62%), Space: 43 MB (14.99%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,45 +1,58 @@
+class Pair{
+    int row,col,time;
+    Pair(int r, int c, int t){
+     this.row=r;
+      this.col=c;
+      this.time=t; 
+    }
+    
+
+}
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int fresh=0;
-        
-        Queue<int[]> bfs=new LinkedList<>();
         int n=grid.length;
         int m=grid[0].length;
+        Queue<Pair> q=new LinkedList<>();
+        int countfresh=0;
+        int[][] vis=new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    bfs.add(new int[]{i,j});
-                    
+                    q.offer(new Pair(i,j,0));
+                    vis[i][j]=2;
                 }
-                else if(grid[i][j]==1){
-                   fresh++;
+                else{
+                    vis[i][j]=0;
 
                 }
-
+                if(grid[i][j]==1) countfresh++;
             }
         }
-       int[][] directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-        int time = 0;
-
-        while (!bfs.isEmpty() && fresh > 0) {
-            int size = bfs.size();
-            for (int i = 0; i < size; i++) {
-                int[] cell = bfs.poll();
-                for (int[] dir : directions) {
-                    int x = cell[0] + dir[0];
-                    int y = cell[1] + dir[1];
-
-                    if (x >= 0 && y >= 0 && x < n && y < m && grid[x][y] == 1) {
-                        grid[x][y] = 2;
-                        bfs.add(new int[]{x, y});
-                        fresh--;
-                    }
+        int cnt=0;
+        int[] drow={-1,0,1,0};
+        int[] dcol={0,1,0,-1};
+        
+        int time =0;
+        while(!q.isEmpty()){
+            Pair p=q.poll();
+            int r=p.row;
+            int c=p.col;
+            int t=p.time;
+            time=Math.max(t,time);
+            for(int i=0;i<4;i++){
+                int rc=r+drow[i];
+                int dc=c+dcol[i];
+                if(rc>=0&&rc<n&&dc>=0&&dc<m&&vis[rc][dc]==0&&grid[rc][dc]==1){
+                    q.offer(new Pair(rc,dc,t+1));
+                    vis[rc][dc]=2;
+                     cnt++;
                 }
+               
             }
-            time++;
-        }
 
-        return fresh == 0 ? time : -1;
+        }
+        if(cnt!=countfresh) return -1;
+        return time;
         
     }
 }
